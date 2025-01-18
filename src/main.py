982fb73836320313ee5dfd4af51a9f0b6b792863
Geronimonetto <geronimomorais1617@gmail.com>
2025-01-18 18:39:18 -0300

    [UPDATE] Ajustando código main para aceitar o multiprocessing

--- src/main.py
+++ src/main.py
@@ -1,18 +1,25 @@
-from utils.connection import DataLoader
+from concurrent.futures import ThreadPoolExecutor
+from utils.connection import load_csv_data, load_db_data
+def main():
+    # Definir arquivos e tabelas para carregar
+    csv_files = ['cliente.csv', 'produtos.csv']
+    db_tables = ['clientes', 'produtos']
 
-if __name__ == "__main__":
-    # Carregar dados de CSV
-    data_loader_csv = DataLoader(source_type='csv', source_path='data')  # Ajustando o caminho para 'data' que está fora de src
-    clientes_df_csv = data_loader_csv.load('cliente.csv')
-    produtos_df_csv = data_loader_csv.load('produtos.csv')
-    
-    # Carregar dados de Banco de Dados
-    data_loader_db = DataLoader(source_type='db', source_path='public')  # Passando 'public' como schema para o banco
-    clientes_df_db = data_loader_db.load('clientes')
-    produtos_df_db = data_loader_db.load('produtos')
-    
+    # Usando multiprocessing com ThreadPoolExecutor
+    with ThreadPoolExecutor() as executor:
+        # Carregar CSV e DB em paralelo
+        csv_results = list(executor.map(load_csv_data, csv_files))
+        db_results = list(executor.map(load_db_data, db_tables))
+        
+    # Desempacotar os resultados
+    clientes_df_csv, produtos_df_csv = csv_results
+    clientes_df_db, produtos_df_db = db_results
+
     # Exemplo de uso
     print(clientes_df_csv.head())  # Exibindo os dados carregados de 'cliente.csv'
     print(produtos_df_csv.head())  # Exibindo os dados carregados de 'produtos.csv'
     print(clientes_df_db.head())   # Exibindo os dados carregados de 'cliente' do banco
     print(produtos_df_db.head())   # Exibindo os dados carregados de 'produtos' do banco
+
+if __name__ == "__main__":
+    main()
