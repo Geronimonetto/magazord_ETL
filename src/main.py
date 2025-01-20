d27cc8c3200a033e0f8b12d65dd7ff866295f7d8
Geronimonetto <geronimomorais1617@gmail.com>
2025-01-19 21:13:57 -0300

    [UPDATE] Versão 0.2 - Incrementei as funções para leitura do banco de dados, a versão 0.1 estava com uma pequena falha, correção feita e testada!!

--- src/main.py
+++ src/main.py
@@ -1,25 +1,37 @@
 from concurrent.futures import ThreadPoolExecutor
 from utils.connection import load_csv_data, load_db_data
-def main():
-    # Definir arquivos e tabelas para carregar
-    csv_files = ['cliente.csv', 'produtos.csv']
-    db_tables = ['clientes', 'produtos']
+
+
+def run():
+    # Arquivos CSV e tabelas do banco de dados a serem lidos
+    csv_files = ['cliente.csv', 'produtos.csv', 'transacoes']
+    db_tables = ['clientes', 'produtos', 'transacoes']
 
     # Usando multiprocessing com ThreadPoolExecutor
     with ThreadPoolExecutor() as executor:
-        # Carregar CSV e DB em paralelo
+        
+        # Carregar dados CSV e DB em paralelo
         csv_results = list(executor.map(load_csv_data, csv_files))
         db_results = list(executor.map(load_db_data, db_tables))
         
     # Desempacotar os resultados
-    clientes_df_csv, produtos_df_csv = csv_results
-    clientes_df_db, produtos_df_db = db_results
+    clientes_df_csv, produtos_df_csv, transacoes_df_csv = csv_results
+    clientes_df_db, produtos_df_db, transacoes_df_db = db_results
 
-    # Exemplo de uso
-    print(clientes_df_csv.head())  # Exibindo os dados carregados de 'cliente.csv'
-    print(produtos_df_csv.head())  # Exibindo os dados carregados de 'produtos.csv'
-    print(clientes_df_db.head())   # Exibindo os dados carregados de 'cliente' do banco
-    print(produtos_df_db.head())   # Exibindo os dados carregados de 'produtos' do banco
+    # Exibir as primeiras linhas de cada DataFrame carregado
+    print("CSV - Cliente:")
+    print(clientes_df_csv.head()) 
+    print("CSV - Produtos:")
+    print(produtos_df_csv.head())  
+    print("CSV - Transações:")
+    print(transacoes_df_csv.head())
+
+    print("DB - Cliente:")
+    print(clientes_df_db.head())  
+    print("DB - Produtos:")
+    print(produtos_df_db.head())  
+    print("DB - Transações:")
+    print(transacoes_df_db.head())   
 
 if __name__ == "__main__":
-    main()
+    run()
