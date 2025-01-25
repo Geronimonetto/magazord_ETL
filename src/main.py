749522948ab0e6b1b22e4b9ae3ac109d9011f2af
Geronimonetto <geronimomorais1617@gmail.com>
2025-01-25 16:16:05 -0300

    feat: Adicionando nova funcionalidade

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
 from concurrent.futures import ThreadPoolExecutor
 from utils.connection import load_csv_data, load_db_data
+from utils.create_tables import execute_sql_file, insert_into_table
+from utils.transform_data import transformar_csv_cliente, transformar_csv_produto, transformar_csv_transacao
+
+output_parquet_cliente = './data/raw/cliente.parquet'
+output_parquet_produto = './data/raw/produto.parquet'
+output_parquet_transacao = './data/raw/transacoes.parquet'
 
 
 def run():
@@ -18,20 +24,22 @@ def run():
     clientes_df_csv, produtos_df_csv, transacoes_df_csv = csv_results
     clientes_df_db, produtos_df_db, transacoes_df_db = db_results
 
-    # Exibir as primeiras linhas de cada DataFrame carregado
-    print("CSV - Cliente:")
-    print(clientes_df_csv.head()) 
-    print("CSV - Produtos:")
-    print(produtos_df_csv.head())  
-    print("CSV - Transações:")
-    print(transacoes_df_csv.head())
+    # Transformar os dados e salvar em Parquet
+    transformar_csv_cliente(clientes_df_csv, output_parquet_cliente) 
+    transformar_csv_produto(produtos_df_csv, output_parquet_produto)  
+    transformar_csv_transacao(transacoes_df_csv, output_parquet_transacao)
 
-    print("DB - Cliente:")
-    print(clientes_df_db.head())  
-    print("DB - Produtos:")
-    print(produtos_df_db.head())  
-    print("DB - Transações:")
-    print(transacoes_df_db.head())   
+    # Inserir os dados transformados no banco de dados
+    insert_into_table(clientes_df_csv, "clientes")  # Nome da tabela no banco
+    insert_into_table(produtos_df_csv, "produtos")  # Nome da tabela no banco
+    insert_into_table(transacoes_df_csv, "transacoes")  # Nome da tabela no banco
+
+
 
 if __name__ == "__main__":
+    # Caminho do arquivo .sql
+    sql_file_path = "./scripts/create_tables.sql"
+    
+    # Executar os comandos SQL do arquivo
+    execute_sql_file(sql_file_path)
     run()
